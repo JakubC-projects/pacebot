564e6e098b3fb23e874ff49cbf36353c234b01cf
Jakub Czyz <jakub12134@gmail.com>
2024-09-16 17:33:56 +0200

    fix milestone values

--- logic/action.go
+++ logic/action.go
@@ -14,8 +14,8 @@ type ActionMilestone struct {
 var milestones = []ActionMilestone{
 	{Date: lo.Must(time.Parse(time.RFC3339, "2024-08-01T00:00:00Z")), Milestone: 70},
 	{Date: lo.Must(time.Parse(time.RFC3339, "2024-09-01T00:00:00Z")), Milestone: 76},
-	{Date: lo.Must(time.Parse(time.RFC3339, "2024-10-01T00:00:00Z")), Milestone: 70},
-	{Date: lo.Must(time.Parse(time.RFC3339, "2024-11-01T00:00:00Z")), Milestone: 82},
+	{Date: lo.Must(time.Parse(time.RFC3339, "2024-10-01T00:00:00Z")), Milestone: 82},
+	{Date: lo.Must(time.Parse(time.RFC3339, "2024-11-01T00:00:00Z")), Milestone: 88},
 	{Date: lo.Must(time.Parse(time.RFC3339, "2024-12-01T00:00:00Z")), Milestone: 94},
 	{Date: lo.Must(time.Parse(time.RFC3339, "2025-01-01T00:00:00Z")), Milestone: 100},
 }
